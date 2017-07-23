abb2d5d663326452b7e2dd38b5217f920fc0efbd
alkrot <graber.qwerty@mail.ru>
2017-07-23 22:09:11 +0300

    Update config.php

--- www/php/config.php
+++ www/php/config.php
@@ -1,10 +1,10 @@
 ﻿<?
-	$dbhost = "localhost";
-	$dbuser = "h94779_smail";
-	$dbpassword = "f5bag67yt";
-	$dbname = "h94779_db_byskynet";
-	$gid = "116246654";
-	$token1 = '6b04519f0c24127de65f1cf75ba024b7544f57a6534dd17dd5dd74d2756a9c5999a897eea5ff74a2c217a';
-	$token2 = '7a4c69ca49058cc6d34b4f4cb061aadf27930b71cc530f3ef338defb2dff16e5e3e1c8445c455a6ef91fd';
-	$token3 = 'e0cc74f3228c92990ddd15868754a51ec25886f22b0ccf31e0104186b9462cc49351d49dc5c9eab455676';
+	$dbhost = "";
+	$dbuser = "";
+	$dbpassword = "";
+	$dbname = "";
+	$gid = "";
+	$token1 = '';
+	$token2 = '';
+	$token3 = ';
 ?>
